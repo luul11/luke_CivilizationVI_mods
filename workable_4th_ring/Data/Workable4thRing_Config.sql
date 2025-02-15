5b6182528c1679b6ee3bfcb5e196ec7d5fb2c1da
LUKE <luul11@163.com>
2025-02-15 10:41:54 +0800

    增加workable_4th_ring
    增加workable_4th_ring

---
+++ workable_4th_ring/Data/Workable4thRing_Config.sql
@@ -0,0 +1,7 @@
+-- Workable4thRing_Config
+-- Author: PhantomJ_M
+-- DateCreated: 4/5/2024 11:11:41 PM
+--------------------------------------------------------------
+UPDATE GameCores SET PackageId = '8f5e69f9-53ea-4dce-b589-b78d21153ad3',	DllPrefix = 'GameCore_Base_Mod'	WHERE GameCore='Base';
+UPDATE GameCores SET PackageId = '8f5e69f9-53ea-4dce-b589-b78d21153ad3',	DllPrefix = 'GameCore_XP1_Mod'	WHERE GameCore='Expansion1';
+UPDATE GameCores SET PackageId = '8f5e69f9-53ea-4dce-b589-b78d21153ad3',	DllPrefix = 'GameCore_XP2_Mod'	WHERE GameCore='Expansion2';
---
+++ workable_4th_ring/Data/Workable4thRing_GlobalParameters.sql
@@ -0,0 +1,5 @@
+-- Workable4thRing_GlobalParameters
+-- Author: PhantomJ_M
+-- DateCreated: 4/5/2024 11:12:11 PM
+--------------------------------------------------------------
+UPDATE GlobalParameters SET Value = '4' WHERE Name = 'CITY_MAX_BUY_PLOT_RANGE';
